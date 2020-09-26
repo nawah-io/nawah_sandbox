2cd932bee918e3d9136169b4f98e7d40fb77ed08
Mahmoud Abduljawad <mahmoud@masaar.com>
2020-09-26 10:41:06 +0400

    Upgraded to Angular 10, Improvements (#1)
    - Replaced obsolete ng-limp SDK.
    - Upgraded to Angular 10, deps.
    - Fixed bug prevented Call panel anon_token input from getting updated Init panel anon_token value.
    - Bumped version to 1.1.0.
    - Updated Pages deployment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,28 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
-import { NgJsonEditorModule } from 'ang-jsoneditor'; 
-
-import { NgLimpModule } from 'ng-limp'
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgJsonEditorModule } from 'ang-jsoneditor';
+import { NgNawahModule } from 'ng-nawah';
 import { AppComponent } from './app.component';
 import { KeysPipe, ValuesPipe } from './object.pipe';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
-  declarations: [
-	AppComponent,
-	KeysPipe,
-	ValuesPipe
-  ],
-  imports: [
-	BrowserModule,
-	FormsModule,
-	ReactiveFormsModule,
-	NgJsonEditorModule,
-	NgLimpModule,
-	NgbModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		KeysPipe,
+		ValuesPipe
+	],
+	imports: [
+		BrowserModule,
+		FormsModule,
+		ReactiveFormsModule,
+		NgJsonEditorModule,
+		NgNawahModule,
+		NgbModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
